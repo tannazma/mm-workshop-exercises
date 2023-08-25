aeefb3742bef985324ed78b0d801ee8c8b9136d3
Aidenbuis <aidenbuis@gmail.com>
2023-08-25 09:58:19 +0200

    Update exercise and solution

--- src/components/exercise/ExerciseTailwindSevenExercise.tsx
+++ src/components/exercise/ExerciseTailwindSevenExercise.tsx
@@ -90,17 +90,17 @@ const Step = ({ step, stepIndex }: StepProps) => {
   return (
     <li
       className={`relative flex items-center py-4 px-6 w-full ${
-        isLastStep ? "" : "border-r"
+        isLastStep ? "" : "md:border-r"
       }`}
     >
       <div
         className={`h-10 w-10 flex items-center justify-center rounded-full border-2 ${
           step.status === "complete"
-            ? "bg-indigo-600 border-indigo-600"
+            ? "bg-indigo-600 border-indigo-600 text-gray-800"
             : step.status === "current"
             ? "bg-white border-indigo-600 text-indigo-600"
             : step.status === "upcoming"
-            ? "border-gray-300 text-gray-400"
+            ? "bg-white border-gray-300 text-gray-400"
             : ""
         }`}
       >
@@ -115,9 +115,9 @@ const Step = ({ step, stepIndex }: StepProps) => {
           step.status === "complete"
             ? "text-gray-800"
             : step.status === "current"
-            ? "text-gray-800"
+            ? "text-indigo-600"
             : step.status === "upcoming"
-            ? "text-gray-800"
+            ? "text-gray-500"
             : ""
         }`}
       >
@@ -125,61 +125,6 @@ const Step = ({ step, stepIndex }: StepProps) => {
       </p>
     </li>
   );
-
-  //   return (
-  //     <li key={step.name} className="relative md:flex md:flex-1">
-  //       {step.status === "complete" ? (
-  //         <span className="flex items-center px-6 py-4 text-sm font-medium">
-  //           <span className="flex h-10 w-10 items-center justify-center rounded-full bg-indigo-600">
-  //             <svg
-  //               xmlns="http://www.w3.org/2000/svg"
-  //               fill="none"
-  //               viewBox="0 0 24 24"
-  //               stroke-width="1.5"
-  //               stroke="currentColor"
-  //               className="h-6 w-6 text-white"
-  //             >
-  //               <path
-  //                 stroke-linecap="round"
-  //                 stroke-linejoin="round"
-  //                 d="M4.5 12.75l6 6 9-13.5"
-  //               />
-  //             </svg>
-  //           </span>
-  //           <span className="ml-4 text-sm font-medium text-gray-900">
-  //             {step.name}
-  //           </span>
-  //         </span>
-  //       ) : step.status === "current" ? (
-  //         <div className="flex items-center px-6 py-4 text-sm font-medium">
-  //           <span className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-indigo-600">
-  //             <span className="text-indigo-600">{step.id}</span>
-  //           </span>
-  //           <span className="ml-4 text-sm font-medium text-indigo-600">
-  //             {step.name}
-  //           </span>
-  //         </div>
-  //       ) : (
-  //         <div className="group flex items-center">
-  //           <span className="flex items-center px-6 py-4 text-sm font-medium">
-  //             <span className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-gray-300 ">
-  //               <span className="text-gray-500 ">{step.id}</span>
-  //             </span>
-  //             <span className="ml-4 text-sm font-medium text-gray-500 ">
-  //               {step.name}
-  //             </span>
-  //           </span>
-  //         </div>
-  //       )}
-
-  //       {/* Separator */}
-  //       {stepIndex !== intitialSteps.length - 1 ? (
-  //         <>
-  //           <div className="absolute right-0 top-0 hidden h-full w-5 md:block border-r border-gray-300" />
-  //         </>
-  //       ) : null}
-  //     </li>
-  //   );
 };
 
 const CheckIcon = () => (
@@ -187,7 +132,7 @@ const CheckIcon = () => (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    stroke-width="3"
     stroke="currentColor"
     className="h-6 w-6 text-white"
   >
--- src/components/solutions/ExerciseTailwindSevenSolution.tsx
+++ src/components/solutions/ExerciseTailwindSevenSolution.tsx
@@ -1,15 +1,74 @@
-const steps = [
-  { id: "01", name: "Job details", status: "complete" },
-  { id: "02", name: "Application form", status: "current" },
-  { id: "03", name: "Preview", status: "upcoming" }
-] as const;
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+interface Step {
+  id: number;
+  name: string;
+  status: "complete" | "current" | "upcoming";
+}
+
+const intitialSteps: Step[] = [
+  { id: 0, name: "Job details", status: "upcoming" },
+  { id: 1, name: "Application form", status: "upcoming" },
+  { id: 2, name: "Preview", status: "upcoming" }
+];
 
 const ExerciseTailwindSevenSolution = () => {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const steps: Step[] = intitialSteps.map((step) => {
+    if (step.id === currentStep) {
+      return { ...step, status: "current" };
+    } else if (step.id < currentStep) {
+      return { ...step, status: "complete" };
+    } else {
+      return step;
+    }
+  });
+
+  const isFirstStep = currentStep === 0;
+  const isLastStep = currentStep === steps.length;
+
   return (
     <nav>
+      <div className="mb-2 flex">
+        <div className="flex border border-gray-300 rounded-md divide-x divide-gray-300">
+          <button
+            type="button"
+            className={`px-4 py-1 cursor-pointer ${
+              isFirstStep
+                ? "text-gray-300 cursor-not-allowed  disabled"
+                : "cursor-pointer hover:bg-gray-100"
+            }`}
+            onClick={() => {
+              if (!isFirstStep) {
+                setCurrentStep(currentStep - 1);
+              }
+            }}
+          >
+            <ArrowLeft className="h-6 w-6 text-gray-300" />
+          </button>
+          <button
+            type="button"
+            className={`px-4 py-1 cursor-pointer ${
+              isLastStep
+                ? "text-gray-300 disabled cursor-not-allowed"
+                : "cursor-pointer hover:bg-gray-100"
+            }`}
+            onClick={() => {
+              if (!isLastStep) {
+                setCurrentStep(currentStep + 1);
+              }
+            }}
+          >
+            <ArrowRight className="h-6 w-6 text-gray-300" />
+          </button>
+        </div>
+      </div>
       <ul className="divide-y divide-gray-300 rounded-md border border-gray-300 md:flex md:divide-y-0">
-        {steps.map((step, stepIdx) => (
-          <Step key={step.id} step={step} stepIdx={stepIdx} />
+        {steps.map((step, stepIndex) => (
+          <Step key={step.id} step={step} stepIndex={stepIndex} />
         ))}
       </ul>
     </nav>
@@ -18,68 +77,101 @@ const ExerciseTailwindSevenSolution = () => {
 
 interface StepProps {
   step: {
-    id: string;
+    id: number;
     name: string;
     status: "complete" | "current" | "upcoming";
   };
-  stepIdx: number;
+  stepIndex: number;
 }
 
-const Step = ({ step, stepIdx }: StepProps) => {
-  return (
-    <li key={step.name} className="relative md:flex md:flex-1">
-      {step.status === "complete" ? (
-        <span className="flex items-center px-6 py-4 text-sm font-medium">
-          <span className="flex h-10 w-10 items-center justify-center rounded-full bg-indigo-600">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="h-6 w-6 text-white"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M4.5 12.75l6 6 9-13.5"
-              />
-            </svg>
-          </span>
-          <span className="ml-4 text-sm font-medium text-gray-900">
-            {step.name}
-          </span>
-        </span>
-      ) : step.status === "current" ? (
-        <div className="flex items-center px-6 py-4 text-sm font-medium">
-          <span className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-indigo-600">
-            <span className="text-indigo-600">{step.id}</span>
-          </span>
-          <span className="ml-4 text-sm font-medium text-indigo-600">
-            {step.name}
-          </span>
-        </div>
-      ) : (
-        <div className="group flex items-center">
-          <span className="flex items-center px-6 py-4 text-sm font-medium">
-            <span className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-gray-300 ">
-              <span className="text-gray-500 ">{step.id}</span>
-            </span>
-            <span className="ml-4 text-sm font-medium text-gray-500 ">
-              {step.name}
-            </span>
-          </span>
-        </div>
-      )}
+const Step = ({ step, stepIndex }: StepProps) => {
+  const isLastStep = stepIndex === intitialSteps.length - 1;
+  const stepNumber = stepIndex + 1;
 
-      {/* Separator */}
-      {stepIdx !== steps.length - 1 ? (
-        <>
-          <div className="absolute right-0 top-0 hidden h-full w-5 md:block border-r border-gray-300" />
-        </>
-      ) : null}
+  return (
+    <li
+      className={`relative flex items-center py-4 px-6 w-full ${
+        isLastStep ? "" : "md:border-r"
+      }`}
+    >
+      <motion.div
+        transition={{
+          duration: 0.2
+        }}
+        initial={false}
+        animate={step.status}
+        variants={{
+          current: {
+            backgroundColor: "var(--white)",
+            color: "var(--indigo-600)",
+            borderColor: "var(--indigo-600)"
+          },
+          upcoming: {
+            backgroundColor: "var(--white)",
+            color: "var(--gray-400)",
+            borderColor: "var(--gray-300)"
+          },
+          complete: {
+            backgroundColor: "var(--indigo-600)",
+            color: "var(--gray-800)",
+            borderColor: "var(--indigo-400)"
+          }
+        }}
+        className={`h-10 w-10 flex items-center justify-center rounded-full border-2`}
+      >
+        {step.status === "complete" ? (
+          <CheckIcon />
+        ) : (
+          <span className="font-semibold">{stepNumber}</span>
+        )}
+      </motion.div>
+      <motion.p
+        animate={step.status}
+        variants={{
+          current: {
+            color: "var(--indigo-600)"
+          },
+          upcoming: {
+            color: "var(--gray-500)"
+          },
+          complete: {
+            color: "var(--gray-800)"
+          }
+        }}
+        className={`text-sm ml-4 font-semibold`}
+      >
+        {step.name}
+      </motion.p>
     </li>
   );
 };
+
+const CheckIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="3"
+    stroke="currentColor"
+    className="h-6 w-6 text-white"
+  >
+    <motion.path
+      initial={{
+        pathLength: 0
+      }}
+      animate={{
+        pathLength: 1
+      }}
+      transition={{
+        delay: 0.2,
+        type: "tween",
+        ease: "easeOut"
+      }}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M4.5 12.75l6 6 9-13.5"
+    />
+  </svg>
+);
 
 export default ExerciseTailwindSevenSolution;
